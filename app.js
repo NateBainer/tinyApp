904e8023788a7da0c5b1d44c7b74a246e482e782
Nathan Bain <nate.bain9@gmail.com>
2022-04-19 14:44:17 -0400

    f dis shiz

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const PORT = 8080;
+
+// a function which handles requests and sends response
+const requestHandler = function(request, response) {
+  console.log('In requestHandler'); // NEW LINE
+  response.end(`Requested Path: ${request.url}\nRequest Method: ${request.method}`);
+};
+
+const server = http.createServer(requestHandler);
+console.log('Welcome'); // NEW LINE
+
+server.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
+
+server.on('connect', () => {
+  console.log('Welcome');
+});
+
+console.log('Last line (after .listen call)');
