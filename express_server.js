cdd15213d90eb78e3bc65d48b4e8fb12cb19c75d
Nathan Bain <nate.bain9@gmail.com>
2022-04-19 23:01:28 -0400

    delete func

--- express_server.js
+++ express_server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.set("view engine", "ejs");
+
 let generateRandomString = (n) => {
   let randomString = '';
   let characters = '0123456789abcdefghijklmnopqrstuvwxyz0123456789';
@@ -13,21 +20,17 @@ let generateRandomString = (n) => {
 };
 
 
-
-
-
-app.set("view engine", "ejs");
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.post("/urls/:shortURL", (req, res) => {
@@ -43,10 +46,6 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/u/:shortURL", (req, res) => {
